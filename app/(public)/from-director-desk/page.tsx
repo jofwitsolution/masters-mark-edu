0f76a8b7b76f204d26c8fc5ca13f94d65537f032
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2024-08-08 22:14:32 +0100

    Add management page

--- app/(public)/from-director-desk/page.tsx
+++ app/(public)/from-director-desk/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Page = () => {
   return (
@@ -10,69 +11,90 @@ const Page = () => {
         </div>
       </div>
       <div className="max-width">
-        <div className="py-8 max-w-[850px]">
-          <div className="w-full flex flex-col md:flex-row gap-4">
-            <div className="">
-              <Image
-                src={"/images/oluwayomi-onabanjo.jpg"}
-                alt="oluwayomi-onabanjo"
-                width={300}
-                height={300}
-              />
+        <div className="flex justify-between">
+          <div className="py-8 max-w-[850px]">
+            <div className="w-full flex flex-col md:flex-row gap-4">
+              <div className="">
+                <Image
+                  src={"/images/oluwayomi-onabanjo.png"}
+                  alt="oluwayomi-onabanjo"
+                  width={300}
+                  height={300}
+                  className=""
+                />
+              </div>
+              <div className="md:max-w-[60%] paragraph-work_sans leading-[28px] text-wrap">
+                <p>
+                  Welcome to MASTER&apos;SMARK School, where we are dedicated to
+                  guiding our students to become outstanding leaders, grounded
+                  in integrity, confidence, and respect. Our main goal is to
+                  provide a comprehensive education that fosters intellectual
+                  curiosity, emotional well-being, and moral strength in each
+                  child.
+                </p>
+                <br />
+                <p>
+                  Safety is our highest priority at MASTER&apos;SMARK School. We
+                  have devoted ourselves to creating a secure and nurturing
+                  environment where everyone is valued and protected. Our
+                  comprehensive safety protocols and diligent supervision ensure
+                  that our school remains a place where students can learn and
+                  thrive without fear or distractions.
+                </p>
+                <br />
+                <p>
+                  Collaboration and teamwork are at the core of our philosophy.
+                  By nurturing a supportive network that includes our qualified
+                  and committed teachers and staff, parents, students, and the
+                  community, we aim to empower each child to reach their fullest
+                  potential.
+                </p>
+              </div>
             </div>
-            <div className="md:max-w-[60%] paragraph-work_sans leading-[28px] text-wrap">
-              <p>
-                Welcome to MASTER&apos;SMARK School, where we are dedicated to
-                guiding our students to become outstanding leaders, grounded in
-                integrity, confidence, and respect. Our main goal is to provide
-                a comprehensive education that fosters intellectual curiosity,
-                emotional well-being, and moral strength in each child.
-              </p>
-              <br />
-              <p>
-                Safety is our highest priority at MASTER&apos;SMARK School. We
-                have devoted ourselves to creating a secure and nurturing
-                environment where everyone is valued and protected. Our
-                comprehensive safety protocols and diligent supervision ensure
-                that our school remains a place where students can learn and
-                thrive without fear or distractions.
-              </p>
-              <br />
-              <p>
-                Collaboration and teamwork are at the core of our philosophy. By
-                nurturing a supportive network that includes our qualified and
-                committed teachers and staff, parents, students, and the
-                community, we aim to empower each child to reach their fullest
-                potential.
-              </p>
-            </div>
+            <p className="paragraph-work_sans leading-[28px]">
+              Respect and integrity are fundamental to our school community. We
+              place great emphasis on the values of mutual respect, honesty, and
+              empathy, preparing our students to become responsible citizens who
+              uphold the highest ethical standards in all aspects of their
+              lives.
+            </p>
+            <br />
+            <p className="paragraph-work_sans leading-[28px]">
+              Our commitment at MASTER&apos;SMARK School extends beyond academic
+              success. We strive to nurture compassionate, resilient, and
+              empathetic individuals who are ready to face the challenges of an
+              ever-changing world. We invite you to join us on this journey of
+              growth, discovery, and transformation.
+            </p>
+            <br />
+            <p className="paragraph-work_sans leading-[28px]">
+              Thank you for entrusting us with the education and nurturing of
+              your child. Together, let&apos;s create an even brighter future
+              for all.
+            </p>
+            <br />
+            <p className="paragraph-work_sans leading-[28px] font-semibold">
+              Oluwayomi Onabanjo
+            </p>
+            <p className="paragraph-work_sans leading-[28px]">
+              Director of School
+            </p>
           </div>
-          <p className="paragraph-work_sans leading-[28px]">
-            Respect and integrity are fundamental to our school community. We
-            place great emphasis on the values of mutual respect, honesty, and
-            empathy, preparing our students to become responsible citizens who
-            uphold the highest ethical standards in all aspects of their lives.
-          </p>
-          <br />
-          <p className="paragraph-work_sans leading-[28px]">
-            Our commitment at MASTER&apos;SMARK School extends beyond academic
-            success. We strive to nurture compassionate, resilient, and
-            empathetic individuals who are ready to face the challenges of an
-            ever-changing world. We invite you to join us on this journey of
-            growth, discovery, and transformation.
-          </p>
-          <br />
-          <p className="paragraph-work_sans leading-[28px]">
-            Thank you for entrusting us with the education and nurturing of your
-            child. Together, let&apos;s create an even brighter future for all.
-          </p>
-          <br />
-          <p className="paragraph-work_sans leading-[28px] font-semibold">
-            Oluwayomi Onabanjo
-          </p>
-          <p className="paragraph-work_sans leading-[28px]">
-            Director of School
-          </p>
+
+          <div className="max-lg:hidden w-[250px] flex flex-col py-8">
+            <Link
+              href={"/about"}
+              className="inline-block py-2 border-b-2 underline text-primary font-medium"
+            >
+              About Us
+            </Link>
+            <Link
+              href={"/management"}
+              className="inline-block py-2 border-b-2 underline text-primary font-medium"
+            >
+              Management
+            </Link>
+          </div>
         </div>
         <div className="border-8 w-full" />
       </div>
--- app/(public)/management/page.tsx
+++ app/(public)/management/page.tsx
@@ -1,7 +1,102 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <section>
+      <div className="bg-tertiary-200 py-4 md:py-10">
+        <div className="max-width">
+          <h1 className="h1-gabriela">School Management Team</h1>
+        </div>
+      </div>
+      <div className="max-width pt-7">
+        <p className="font-medium font-inter">
+          Meet the management team dedicated to continue improving
+          Master&apos;sMark day by day, year on year.
+        </p>
+
+        <div className="flex justify-between">
+          <div className="mt-10 flex flex-wrap gap-8 max-w-[700px]">
+            <div className="flex flex-col gap-2 w-max">
+              <Image
+                src={"/images/oluwayomi-onabanjo-short.png"}
+                alt="director"
+                width={300}
+                height={240}
+                className="w-[300px] h-[240px]"
+              />
+              <div className="flex flex-col gap-1">
+                <h2 className="h3-semibold">Oluwayomi Onabanjo</h2>
+                <span>Director</span>
+              </div>
+              <div className="border-2" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-max">
+              <Image
+                src={"/images/arc-olugbenga-onabanjo.png"}
+                alt="director"
+                width={300}
+                height={240}
+                className="w-[300px] h-[240px]"
+              />
+              <div className="flex flex-col gap-1">
+                <h2 className="h3-semibold">ARC Olugbenga Onabanjo</h2>
+                <span>Chairman</span>
+              </div>
+              <div className="border-2" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-max">
+              <Image
+                src={"/images/mrs-olufunmilola-oshinloye.png"}
+                alt="head-of-education"
+                width={300}
+                height={240}
+                className="w-[300px] h-[240px]"
+              />
+              <div className="flex flex-col gap-1">
+                <h2 className="h3-semibold">Mrs Olufunmiloa Oshinloye</h2>
+                <span>Head of Education</span>
+              </div>
+              <div className="border-2" />
+            </div>
+
+            <div className="flex flex-col gap-2 w-max">
+              <Image
+                src={"/images/ayokunle-okunowo.png"}
+                alt="senior-admin-personel"
+                width={300}
+                height={240}
+                className="w-[300px] h-[240px]"
+              />
+              <div className="flex flex-col gap-1">
+                <h2 className="h3-semibold">Mr Ayokunle Okunowo</h2>
+                <span>Senior Admin Personnel</span>
+              </div>
+              <div className="border-2" />
+            </div>
+          </div>
+
+          <div className="max-lg:hidden w-[250px] flex flex-col py-8">
+            <Link
+              href={"/about"}
+              className="inline-block py-2 border-b-2 underline text-primary font-medium"
+            >
+              About Us
+            </Link>
+            <Link
+              href={"/from-director-desk"}
+              className="inline-block py-2 border-b-2 underline text-primary font-medium"
+            >
+              The director&apos;s desk
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Page;
